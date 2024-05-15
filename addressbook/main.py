21f33066403c3aac5c2a97ed25baaeadc230f3bd
Leoleonish <157866087+Leoleonish@users.noreply.github.com>
2024-05-15 16:39:07 +0530

    Update main.py

--- addressbook/main.py
+++ addressbook/main.py
@@ -56,3 +56,28 @@ async def destroy(id, db: Session = Depends(get_db)):
     db.commit()
     return {"data": 'empty'}
 
+
+def calculate_distance(lat1,lon1,lat2,lon2):
+    from math import radians, sin, cos, acos, sqrt
+
+    R = 6371      #radius of the world in km
+
+    lat1, lon1 = radians(lat1), radians(lon1)
+    lat2, lon2 = radians(lat2), radians(lon2)
+
+    distance_lat = lat2 - lat1
+    distance_lon = lon1 - lon2
+
+    a = sin(distance_lat / 2) * sin(distance_lon / 2) + cos(lat1) * cos(lat2) * sin(distance_lon / 2) * sin(distance_lon / 2)
+    c = 2 * acos(sqrt(a))
+
+    return R * c
+
+
+@app.get('/address/near', response_model=List[Address])
+async def get_address_near(latitude: float, longitude: float, radius: float, db: Session = Depends(get_db)):
+    addresses = db.query(models.Address).all()
+    return [
+        address for address in addresses if calculate_distance(latitude, longitude, address.longitude, address.longitude) <= radius
+    ]
+
